b1988ce9344f45dd44abb0a259dd1c88de819433
vincenzoschiavone <vincenzo_schiavone@icloud.com>
2019-03-11 11:15:02 +0100

    Basic Exercise 04 for loop done

--- BasicExercise04.php
+++ BasicExercise04.php
@@ -3,7 +3,22 @@
 <head>
 	<title>Basic Exercise 04</title>
 </head>
-<body>
+<body style="text-align: center">
+
+	<?php 
+
+		$names = "Vincenzo";
+		$names2 = "Jamie";
+		$num = 1;
+		for ($i=1; $i<=50; $i=$i+1){
+
+			echo $num++; echo " $names<br>";
+			echo "$i $names2 <br>";
+			
+		}
+
+		
+	?>
 
 </body>
 </html>
