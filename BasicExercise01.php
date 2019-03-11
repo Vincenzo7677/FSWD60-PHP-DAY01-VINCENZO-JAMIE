f3c693094458db61f52a778fa0f5fb172c4b2da2
vincenzoschiavone <vincenzo_schiavone@icloud.com>
2019-03-11 10:02:39 +0100

    Basic Exercise 01 done

---
+++ BasicExercise01.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="BasicExercise.css">
+	<title>Basic Exercise 01</title>
+</head>
+<body>
+
+
+	<?php
+
+	$name = "Jamie Slaats";
+	$name2 = "Vincenzo Schiavone";
+
+	echo ("<h1> $name </h1> <br>") ;
+	echo ("<h1> $name2 </h1>");
+
+
+	 ?>
+</body>
+</html>
