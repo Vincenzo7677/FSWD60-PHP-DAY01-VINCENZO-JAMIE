e0e0ed845679b59d21c6d6fa8da1576962f695d9
vincenzoschiavone <vincenzo_schiavone@icloud.com>
2019-03-11 13:01:04 +0100

    Basic Exercise 05 done

---
+++ BasicExercise05.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Basic Exercise 05</title>
+</head>
+<body style="text-align: center;">
+
+	<?php 
+
+			$array = array(1,2,3,4,5,6,7,8,9,10);
+
+			foreach ($array as $value) {
+				
+
+				echo "$value <br>";
+			}
+
+
+	?>
+
+</body>
+</html>
