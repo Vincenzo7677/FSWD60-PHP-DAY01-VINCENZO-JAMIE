04be224ccf1207384cd3521df27e21ef2347dfa1
Jamie <jjslaats@mailbox.org>
2019-03-11 11:53:54 +0100

    commit of part 2 and 3

---
+++ BasicExercise04_part2.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Basic Exercise 04</title>
+</head>
+<body style="text-align: center">
+
+	<?php 
+
+	$names = "Jamie & Vincenzo";
+	$x = 1;
+		
+		while($x <= 50){
+
+			echo "$x $names <br>";
+			$x++;
+			
+		}
+
+		
+	?>
+
+</body>
+</html>
---
+++ BasicExercise04_part3.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Basic Exercise 04</title>
+</head>
+<body style="text-align: center">
+
+	<?php 
+
+	$names = "Jamie & Vincenzo";
+	$x = 0;
+	do {
+	$x++;
+	echo "$x $names <br>";	
+	}	
+	while($x < 50);
+		
+	?>
+
+</body>
+</html>
