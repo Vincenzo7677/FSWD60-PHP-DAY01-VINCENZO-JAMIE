1d9858e45de24bd37d585a5d7958d6538df5074e
vincenzoschiavone <vincenzo_schiavone@icloud.com>
2019-03-11 13:27:11 +0100

    add basic exercise 6

---
+++ BasicExercise06.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>test</title>
+</head>
+<body>
+
+	<?php 
+
+			$array = array(
+
+				"Alien" => array(
+
+					"img" => "<img src=''>",
+					"name"=> "Alien",
+					"age" => 65,
+					"country"=> "Planet Zoran"
+					
+					),
+
+				"Bugsbunny" => array(
+
+					"img" => "<img src=''>",
+					"name"=> "Bugsbunny",
+					"age" => 90,
+					"country"=> "Planet Boro"
+					
+					),
+
+				"Daffy Duck" => array(
+
+					"img" => "<img src=''>",
+					"name"=> "Daffy Duck",
+					"age" => 89,
+					"country"=> "Planet Moron"
+					
+					),
+
+				"Roadrunner" => array(
+
+					"img" => "<img src=''>",
+					"name"=> "Roadrunnern",
+					"age" => 78,
+					"country"=> "Planet Zippi"
+					
+					),
+
+				"Tweetybird" => array(
+
+					"img" => "<img src=''>",
+					"name"=> "Tweetybird",
+					"age" => 80,
+					"country"=> "Planet Nokitties"
+					
+					),
+				"Speedy Gonzales" => array(
+
+					"img" => "<img src=''>",
+					"name"=> "Speedy Gonzales",
+					"age" => 60,
+					"country"=> "Planet Mechiko"
+					
+					)
+
+			);
+
+	?>
+
+</body>
+</html>
